022419c40dc167b7d65e5c9011d78c7e0c4f78b8
Fajar Hari Prabowo <me.fhprabowo@gmail.com>
2021-11-28 08:13:26 +0700

    update with templates

--- index.php
+++ index.php
@@ -1,25 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php include 'templates/header.php'; ?>
+<header>
+    <h1 class="header">abiumik.id</h1>
+</header>
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,900;1,100;1,400;1,900&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="css/style.css">
-
-    <title>abiumikid</title>
-</head>
-<body>
-    <header>
-        <h1 class="header">abiumik.id</h1>
-    </header>
-
-    <main class="body">
-        <p class="desc">sebuah masterpiece yang ... <span class="bagong mbagongkan">membagongkann</span></p>
-    </main>
-</body>
-</html>
+<main class="body">
+    <p class="desc">sebuah masterpiece yang ... <span class="bagong mbagongkan">membagongkann</span></p>
+</main>
+<?php include 'templates/footer.php'; ?>
