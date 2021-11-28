ef17344a37cc7ce0216595ce29d68137fab06776
Fajar Hari Prabowo <me.fhprabowo@gmail.com>
2021-11-28 08:17:10 +0700

    fix web title

--- templates/header.php
+++ templates/header.php
@@ -5,12 +5,14 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<!-- google font -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,900;1,100;1,400;1,900&display=swap" rel="stylesheet">
 
+<!-- main style -->
 <link rel="stylesheet" href="css/style.css">
 
-<title>abiumikid</title>
+<title>abiumik.id</title>
 </head>
 <body>
