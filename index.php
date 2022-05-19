3d2970c088ffd7e3d4b37bae492faaac9f09e370
Fajar Hari Prabowo <me.fhprabowo@gmail.com>
2022-05-19 12:33:47 +0700

    initial

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php include 'templates/header.php'; ?>
-<header>
-    <h1 class="header">abiumik.id</h1>
-</header>
-
-<main class="body">
-    <p class="desc">sebuah masterpiece yang ... <span class="bagong mbagongkan">membagongkann</span></p>
-</main>
-<?php include 'templates/footer.php'; ?>
